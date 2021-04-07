d92d22377f0c4bd29cbeb3c043947e0aaefd6e49
Karolina Mamczarz <karomamczi@gmail.com>
2021-04-08 00:56:22 +0200

    feat: Render list of issues, attach styling

--- routes/issue-tracker.js
+++ routes/issue-tracker.js
@@ -3,7 +3,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',  (req, res, next) => {
-  res.render('issue-tracker');
+  const issues = [
+    {
+      title: 'Issue 1',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      status: 'open',
+    },
+    {
+      title: 'Issue 2',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      status: 'pending',
+    },
+    {
+      title: 'Issue 3',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      status: 'closed',
+    }
+  ];
+  res.render('issue-tracker', {
+    issues,
+  });
 });
 
 module.exports = router;
