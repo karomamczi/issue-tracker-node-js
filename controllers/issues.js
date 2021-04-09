d062fa1f4507b322ef6be8c17033fec00d036089
Karolina Mamczarz <karomamczi@gmail.com>
2021-04-09 22:19:35 +0200

    feat: Update issue status controller function

--- controllers/issues.js
+++ controllers/issues.js
@@ -1,11 +1,21 @@
 const Issue = require('../models/issue');
 
-exports.getIssueTracker = (req, res, next) => {
-  Issue.fetchAll().then((issues) => {
-    res.render('issue-tracker', {
-      issues,
-    });
+exports.getIssueTracker = async (req, res) => {
+  const issues = await Issue.fetchAll();
+  res.render('issue-tracker', {
+    issues,
   });
 };
 
-exports.updateIssueStatus = (req, res, next) => {};
+exports.updateIssueStatus = async (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+
+  try {
+    await Issue.saveNewStatus(id, body.status);
+  } catch (e) {
+    res.status(e.code).send(e.errors.join(','));
+  } finally {
+    res.end();
+  }
+};
